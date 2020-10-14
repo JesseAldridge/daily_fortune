bf5fcab89a07d3ebf55082dd9db78bc5b39be3ec
Jesse Aldridge <jessealdridge@gmail.com>
2020-10-14 11:37:07 -0700

    preformatted

--- main.js
+++ main.js
@@ -4,20 +4,21 @@ const ChildProcess = require('child_process');
 const expand_home_dir = require('expand-home-dir')
 const Discord = require('discord.js');
 
+
 const config_path = expand_home_dir('~/discord.json')
 const config = JSON.parse(fs.readFileSync(config_path))
 
 const client = new Discord.Client();
 
-
 function send_message_loop() {
   client.guilds.cache.forEach((guild) => {
     const channel = guild.channels.cache.find(ch => ch.name === 'general');
     // Do nothing if the channel wasn't found on this server
-    if (!channel) return;
+    if (!channel)
+      return;
 
     const result = ChildProcess.spawnSync('fortune', [], { encoding : 'utf8' });
-    channel.send(result.output[1]);
+    channel.send('```\n' + result.output[1] + '\n```');
   })
 
   setTimeout(send_message_loop, 1000 * 60 * 60 * 24);
