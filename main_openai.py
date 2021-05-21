4862e4044dab9054e87489acc6c94921b5bd7732
Jesse Aldridge <jessealdridge@gmail.com>
2021-05-21 13:45:49 -0500

    channel.send

--- main_openai.py
+++ main_openai.py
@@ -36,7 +36,7 @@ async def on_ready(*a, **kw):
     print('guild:', guild)
     for channel in guild.channels:
       print('channel:', channel)
-      await channel.set("*bot rebooted*")
+      await channel.send("*bot rebooted*")
 
 @client.event
 async def on_message(message):
