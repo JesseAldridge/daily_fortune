9363213853d192b7ba76846062afd55b9d2c29d3
Jesse Aldridge <jessealdridge@gmail.com>
2021-05-20 23:02:46 -0500

    reboot

--- main_openai.py
+++ main_openai.py
@@ -34,6 +34,8 @@ async def on_message(message):
 
   if message.content.strip().endswith('?'):
     await answer_question(message)
+  elif message.content = '_reboot':
+    g.recent_messages = []
   else:
     roll = random.random()
     print('roll:', roll)
